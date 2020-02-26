d8ded235c889b85bccf1c63035b292cb85c8aba9
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-02-26 05:17:48 +0100

    Integrate Retrofit and RxJava to get remote data

---
+++ app/src/main/java/com/demoapp/jatinsinghsaluja/dogs/model/DogsApiService.kt
@@ -0,0 +1,25 @@
+package com.demoapp.jatinsinghsaluja.dogs.model
+
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class DogsApiService {
+
+    private val BASE_URL = "https://raw.githubusercontent.com"
+
+    private val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+            //converts list of DogBreed objects to Observable of type Single
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+        .create(DogsApi::class.java)
+
+    fun getDogs(): Single<List<DogBreed>> {
+        return api.getDogs()
+
+    }
+
+}
--- app/src/main/java/com/demoapp/jatinsinghsaluja/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/demoapp/jatinsinghsaluja/dogs/viewmodel/ListViewModel.kt
@@ -3,8 +3,18 @@ package com.demoapp.jatinsinghsaluja.dogs.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.demoapp.jatinsinghsaluja.dogs.model.DogBreed
+import com.demoapp.jatinsinghsaluja.dogs.model.DogsApiService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel: ViewModel() {
+
+    private val dogsApiService = DogsApiService()
+
+    // allows to observe the observable of type Single returned by the remote API
+    private val disposable = CompositeDisposable()
 
     val dogs = MutableLiveData<List<DogBreed>>()
     val dogsLoadingError = MutableLiveData<Boolean>()
@@ -14,17 +24,43 @@ class ListViewModel: ViewModel() {
 
     fun refresh(){
 
-        val dog1 = DogBreed("1","Corgi", "15 years",
-            "breed group", "bred for", "temperament", "")
-        val dog2 = DogBreed("2","Labrador", "18 years",
-            "breed group", "bred for", "temperament", "")
-        val dog3 = DogBreed("3","Rotwailer", "20 years",
-            "breed group", "bred for", "temperament", "")
+        fetchFromRemote()
 
-        val dogsList = arrayListOf<DogBreed>(dog1, dog2, dog3)
+    }
 
-        dogs.value =dogsList
-        dogsLoadingError.value = false
-        loading.value = false
+    private fun fetchFromRemote(){
+
+        loading.value = true
+        disposable.add(
+            dogsApiService.getDogs()
+                    //access remote api on background thread
+                .subscribeOn(Schedulers.newThread())
+                    //process the response on main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object:DisposableSingleObserver<List<DogBreed>>(){
+                    override fun onSuccess(t: List<DogBreed>) {
+
+                        dogs.value = t
+                        dogsLoadingError.value = false
+                        loading.value = false
+
+                    }
+
+                    override fun onError(e: Throwable) {
+
+                        dogsLoadingError.value = true
+                        loading.value = false
+                        e.printStackTrace()
+
+                    }
+
+                })
+        )
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
