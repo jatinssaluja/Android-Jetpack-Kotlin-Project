51053e522647864660b8c40991aaa9301cdade12
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-03-02 00:40:48 +0100

    Add menu items in DetailFragment for sending SMS and sharing content

--- app/src/main/java/com/demoapp/jatinsinghsaluja/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/demoapp/jatinsinghsaluja/dogs/view/DetailFragment.kt
@@ -4,10 +4,8 @@ package com.demoapp.jatinsinghsaluja.dogs.view
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -43,6 +41,9 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
+
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         // Inflate the layout for this fragment
         return dataBinding.root
@@ -99,6 +100,23 @@ class DetailFragment : Fragment() {
 
         })
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_send_sms -> {
+
+            }
+            R.id.action_share -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
