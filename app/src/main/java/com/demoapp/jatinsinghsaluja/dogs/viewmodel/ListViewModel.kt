3f2bb4f99ed64fbbfcef39f55aee4925473a7952
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-03-01 01:10:18 +0100

    Refactor code to store data into Room Database

--- app/src/main/java/com/demoapp/jatinsinghsaluja/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/demoapp/jatinsinghsaluja/dogs/viewmodel/ListViewModel.kt
@@ -1,15 +1,19 @@
 package com.demoapp.jatinsinghsaluja.dogs.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.demoapp.jatinsinghsaluja.dogs.model.DogBreed
+import com.demoapp.jatinsinghsaluja.dogs.model.DogDatabase
 import com.demoapp.jatinsinghsaluja.dogs.model.DogsApiService
+import com.demoapp.jatinsinghsaluja.dogs.util.SharedPreferencesHelper
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class ListViewModel: ViewModel() {
+class ListViewModel(application: Application): BaseViewModel(application) {
 
     private val dogsApiService = DogsApiService()
 
@@ -21,6 +25,8 @@ class ListViewModel: ViewModel() {
 
     // this live data will tell its subscribers that the system is loading
     val loading = MutableLiveData<Boolean>()
+
+    private var prefHelper = SharedPreferencesHelper(getApplication())
 
     fun refresh(){
 
@@ -38,11 +44,9 @@ class ListViewModel: ViewModel() {
                     //process the response on main thread
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object:DisposableSingleObserver<List<DogBreed>>(){
-                    override fun onSuccess(t: List<DogBreed>) {
+                    override fun onSuccess(dogList: List<DogBreed>) {
 
-                        dogs.value = t
-                        dogsLoadingError.value = false
-                        loading.value = false
+                        storeDogsLocally(dogList)
 
                     }
 
@@ -56,6 +60,31 @@ class ListViewModel: ViewModel() {
 
                 })
         )
+
+    }
+
+    private fun dogsFetched(dogList:List<DogBreed>){
+
+        dogs.value = dogList
+        dogsLoadingError.value = false
+        loading.value = false
+
+    }
+
+    private fun storeDogsLocally(dogList:List<DogBreed>){
+
+        launch {
+            val dogDao = DogDatabase(getApplication()).dogDao()
+            dogDao.deleteAllDogs()
+            val result = dogDao.insertAll(*dogList.toTypedArray())
+            var i = 0
+            while (i<dogList.size){
+                dogList[i].uuid = result[i].toInt()
+                ++i
+            }
+            dogsFetched(dogList)
+        }
+        prefHelper.saveUpdateTime(System.nanoTime())
 
     }
 
